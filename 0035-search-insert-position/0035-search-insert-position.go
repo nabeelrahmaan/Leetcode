c152b9800820fa4c141e5adbb4946f077d7687b9
Nabeel <nabeelrahman246@gmail.com>
2026-02-13 14:20:30 +0530

    Time: 0 ms (100%), Space: 4.7 MB (77.01%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.go
@@ -0,0 +1,15 @@
+func searchInsert(nums []int, target int) int {
+    low := 0
+    high := len(nums)-1
+    for low<=high {
+        mid := (high+low)/2
+        if nums[mid] == target {
+            return mid
+        }else if nums[mid] < target {
+            low = mid+1
+        }else{
+            high = mid-1
+        }
+    }
+    return low
+}
